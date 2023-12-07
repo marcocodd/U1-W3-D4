c5b769f7c4b4a21532d6aadb60ac45b0d69087b2
marcocodd <brunetti.mrco@gmail.com>
2023-12-07 17:50:48 +0100

    piccoloAvanzamento

--- ScriptTombola/script.js
+++ ScriptTombola/script.js
@@ -2,7 +2,7 @@ const tab = document.getElementById("divTabella");
 const numberOfTheCell = document.querySelector("h3");
 
 const tabCreation = function () {
-  for (i = 1; i <= 90; i++) {
+  for (let i = 1; i <= 90; i++) {
     const cell = document.createElement("div");
     cell.classList.add("divNumbers");
     const numberOfTheCell = document.createElement("h3");
@@ -16,19 +16,17 @@ tabCreation();
 
 //funzione per chiamare numeri random da 1 a 90 prova per vedere se il bottone va, ma servirà un array di numeri da 1 a90 per non chiamare mai lo stesso numero...
 
-const callRandomNumber = function () {
-  const CalledNumber = Math.floor(Math.random() * 90) + 1;
-
-  console.log(CalledNumber);
-};
-
-const button = document.getElementById("callButton");
-
-button.addEventListener("click", callRandomNumber);
-
 const Numbers = [];
 
-for (i = 1; i < 90; i++) {
+for (i = 1; i <= 90; i++) {
   Numbers.push(i);
 }
 console.log(Numbers);
+
+const callRandomNumber = function () {
+  const CalledNumber = Numbers[Math.floor(Math.random() * 90) + 1];
+  console.log(CalledNumber);
+};
+const button = document.getElementById("callButton");
+
+button.addEventListener("click", callRandomNumber);
